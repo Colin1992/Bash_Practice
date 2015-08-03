845292b6710d5fa929c15da8aac37ded6a595d8f
colin1992 <colinhamilton579@gmail.com>
2015-08-03 11:05:04 -0600

    Same script as updatedb.sh except implementing the db export sub-command of wpcli instead of --update-all

---
+++ exportdb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+# Script to update all wordpress sites in the public_html
+
+
+# Help flag
+if [ "$1" == "-h" ];
+then
+	echo ""
+	echo "Site's won't update unless wordpress command line is installed with the alias 'wpcli'."
+	echo ""
+	exit 0
+fi
+
+
+#  Bash variable to find all wordpress sites
+configfiles=$(find2perl ~/public_html/ -type f -name wp-config.php | perl)
+
+for y in $configfiles;
+do
+	# Grab the directory without the file at the end so that the script can then change directories into it
+	x=$(echo $y | grep -Eo "^(.*/.*/)(\1)*")		
+	cd $x
+	# if-statement to see if it was able to change directories
+	if [[ $? == 0 ]];
+	then
+		# wpcli error doesn't register as an error, for that reason I had to run grep after for the if-statement
+		wpcli db export 2>/dev/null | grep -i "success" >/dev/null 2>&1
+		# if-statement to see if the command can be run successfully
+		if [[ $? == 0 ]];
+		then
+			echo "$x exported successfully!"
+		else
+			echo "Unable to export $x!" >&2
+		fi
+	else
+		echo "Unable to move into $x!" >&2
+	fi
+done
